e453ebf02ab8bc136dfae4871529a2232afc5225
padyalkar <adyalkarp@gmail.com>
2022-03-22 22:52:49 +0000

    fp

--- app-Copy1.py
+++ app-Copy1.py
@@ -18,11 +18,11 @@ githublink = 'https://github.com/plotly-dash-apps/304-titanic-dropdown'
 
 
 ###### Import a dataframe #######
-df = pd.read_csv('assets/football.csv')
-#df['Female']=df['Sex'].map({'male':0, 'female':1})
-#df['Cabin Class'] = df['Pclass'].map({1:'first', 2: 'second', 3:'third'})
-labels=['Most home goals']#, 'Most away goals', 'Top 10 city hosting tournaments', 'Top 10 countries hosting tournaments']
-values=[{'groubyby':'home_team','column':'home_score', 'function':'sum()'}]
+df = pd.read_csv("https://raw.githubusercontent.com/austinlasseter/plotly_dash_tutorial/master/00%20resources/titanic.csv")
+df['Female']=df['Sex'].map({'male':0, 'female':1})
+df['Cabin Class'] = df['Pclass'].map({1:'first', 2: 'second', 3:'third'})
+variables_list=['Survived', 'Female', 'Fare', 'Age']
+
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -34,8 +34,8 @@ app.layout = html.Div([
     html.H3('Choose a continuous variable for summary statistics:'),
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': i, 'value': j} for i,j in zip(labels,values)],
-        value=values[0]
+        options=[{'label': i, 'value': i} for i in variables_list],
+        value=variables_list[0]
     ),
     html.Br(),
     dcc.Graph(id='display-value'),
@@ -49,36 +49,35 @@ app.layout = html.Div([
 @app.callback(Output('display-value', 'figure'),
               [Input('dropdown', 'value')])
 def display_value(continuous_var):
-    
-    # grouped_mean=df.groupby(['Cabin Class', 'Embarked'])[continuous_var].mean()
-    # results=pd.DataFrame(grouped_mean)
-    # # Create a grouped bar chart
-    # mydata1 = go.Bar(
-    #     x=results.loc['first'].index,
-    #     y=results.loc['first'][continuous_var],
-    #     name='First Class',
-    #     marker=dict(color=color1)
-    # )
-    # mydata2 = go.Bar(
-    #     x=results.loc['second'].index,
-    #     y=results.loc['second'][continuous_var],
-    #     name='Second Class',
-    #     marker=dict(color=color2)
-    # )
-    # mydata3 = go.Bar(
-    #     x=results.loc['third'].index,
-    #     y=results.loc['third'][continuous_var],
-    #     name='Third Class',
-    #     marker=dict(color=color3)
-    # )
+    grouped_mean=df.groupby(['Cabin Class', 'Embarked'])[continuous_var].mean()
+    results=pd.DataFrame(grouped_mean)
+    # Create a grouped bar chart
+    mydata1 = go.Bar(
+        x=results.loc['first'].index,
+        y=results.loc['first'][continuous_var],
+        name='First Class',
+        marker=dict(color=color1)
+    )
+    mydata2 = go.Bar(
+        x=results.loc['second'].index,
+        y=results.loc['second'][continuous_var],
+        name='Second Class',
+        marker=dict(color=color2)
+    )
+    mydata3 = go.Bar(
+        x=results.loc['third'].index,
+        y=results.loc['third'][continuous_var],
+        name='Third Class',
+        marker=dict(color=color3)
+    )
 
     mylayout = go.Layout(
-        title= continuous_var,
+        title='Grouped bar chart',
         xaxis = dict(title = 'Port of Embarkation'), # x-axis label
-        yaxis = dict(title = 'lol'), # y-axis label
+        yaxis = dict(title = str(continuous_var)), # y-axis label
 
     )
-    fig = go.Figure(layout=mylayout)
+    fig = go.Figure(data=[mydata1, mydata2, mydata3], layout=mylayout)
     return fig
 
 
